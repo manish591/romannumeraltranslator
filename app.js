03c64125775dfb7feeeb0fabec6470f46a40c838
manish591 <manishdevrani777@gmail.com>
2021-06-14 23:44:24 +0530

    updated app.js

---
+++ app.js
@@ -0,0 +1,28 @@
+const inputNumberHere = document.querySelector('.inputnumber');
+const convertToNumeral = document.querySelector('.convertbutton');
+const outputPart = document.querySelector('.outputromannumeral');
+
+const romanURL = 'https://api.funtranslations.com/translate/roman-numerals.json';
+
+function getURL (number) {
+  return romanURL + "?" + "text=" + number;
+}
+
+function errorHandler () {
+    console.log('Error Occured!');
+    alert('Something Went Wrong!');
+}
+
+function getRomanValue () {
+    let input = inputNumberHere.value;
+
+    fetch(getURL(input))
+    .then(reply => reply.json())
+    .then(dataLoaded => {
+        let givenRomanValue = dataLoaded.contents.translated;
+        console.log(givenRomanValue);
+    })
+    .catch(errorHandler);
+}
+
+convertToNumeral.addEventListener('click', getRomanValue);
